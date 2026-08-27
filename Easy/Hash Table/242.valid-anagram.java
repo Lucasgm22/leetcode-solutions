ca7cb5c1211602e43cc20baa66290e453d5335c7
Lucas Miranda <lucasgm22@gmail.com>
2026-08-27 19:20:29 -0300

    Sync: 1 solutions - 2026-08-27 22:20:28
    - [242. valid-anagram] Runtime: 5 ms (beats 62.40%), Memory: 44.1 MB (beats 89.66%)

--- Easy/Hash Table/242.valid-anagram.java
+++ Easy/Hash Table/242.valid-anagram.java
@@ -35,23 +35,22 @@
  * Follow up: What if the inputs contain Unicode characters? How would
  * you adapt your solution to such a case?
  *
- * O(|s| + |t|) in time
+ * O(|s|) in time
  * O(1) in space
 */
 
 class Solution {
-    private static int ASCII_TABLE_SIZE = 128;
+    private static int ALPHABET_SIZE = 26;
 
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
-        int[] lookup = new int[ASCII_TABLE_SIZE];
+        int[] lookup = new int[ALPHABET_SIZE];
 
-        for (int i = 0; i < s.length(); i++)
-            lookup[s.charAt(i)]++;
-
-        for (int i = 0; i < t.length(); i++)
-            lookup[t.charAt(i)]--;
+        for (int i = 0; i < s.length(); i++) {
+            lookup[s.charAt(i) - 'a']++;
+            lookup[t.charAt(i) - 'a']--;
+        }
 
         for (int count: lookup)
             if (count > 0)

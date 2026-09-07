7d2b237718e8b95e87414cf329684f6d12d155b1
Lucas Miranda <lucasgm22@gmail.com>
2026-09-07 12:35:56 -0300

    Sync: 1 solutions - 2026-09-07 15:35:53
    - [206. reverse-linked-list] Runtime: 0 ms (beats 100.00%), Memory: 44.3 MB (beats 30.79%)

--- Easy/Linked List/206.reverse-linked-list.java
+++ Easy/Linked List/206.reverse-linked-list.java
@@ -56,7 +56,7 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
 
-        while(head != null) {
+        while (head != null) {
             ListNode aux = head.next;
             head.next = prev;
             prev = head;

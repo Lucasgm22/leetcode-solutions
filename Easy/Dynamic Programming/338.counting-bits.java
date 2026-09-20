d06a747c605a52fa3fccd1734d0df8c24199f07f
Lucas Miranda <lucasgm22@gmail.com>
2026-09-20 10:05:37 -0300

    Sync: 1 solutions - 2026-09-20 13:05:37

---
+++ Easy/Dynamic Programming/338.counting-bits.java
@@ -0,0 +1,73 @@
+/*
+ * 338. Counting Bits
+ * Difficulty: Easy
+ * https://leetcode.com/problems/counting-bits/
+ *
+ * ──────────────────────────────────────────────────
+ *
+ * Given an integer n, return an array ans of length n + 1 such that for
+ * each i (0 <= i <= n), ans[i] is the number of 1's in the binary
+ * representation of i.
+ *
+ * Do not solve it with built-in functions (i.e., like
+ * __builtin_popcount in C++).
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: [0,1,1]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ *
+ * Example 2:
+ *
+ * Input: n = 5
+ * Output: [0,1,1,2,1,2]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 	• 0 <= n <= 10^5
+ *
+ *
+ *
+ * Follow up:
+ *
+ * • It is very easy to come up with a solution with a runtime of O(n
+ * log n). Can you do it in linear time O(n) and possibly in a single
+ * pass?
+ *
+ * O(n) in time
+ * O(1) in space
+*/
+
+class Solution {
+    public int[] countBits(int n) {
+        if (n == 0)
+            return new int[] { 0 };
+
+        if (n == 1)
+            return new int[] { 0, 1 };
+
+
+        int[] ans = new int[n+1];
+        ans[0] = 0;
+        ans[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            ans[i] = ans[(i >> 1)] + ((i % 2) == 1 ? 1 : 0);
+
+        return ans;
+    }
+}
